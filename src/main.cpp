357817105574731fc393b85b97e7789113dcc645
Daniel Dakhno <dakhnod@gmail.com>
2025-03-20 01:43:16 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,280 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <map>
+
+bool uartBufferAvailable = false;
+
+typedef std::function<void (uint8_t, uint8_t, uint16_t)> instructionHandler;
+typedef std::map<uint8_t, instructionHandler> classHandler;
+
+uint8_t lastClass, lastInstruction;
+
+uint8_t stub[64];
+
+std::map<uint8_t, int> pinMapping {
+  {1, PIN_RESET},
+  {2, PIN_IRQ},
+  {5, PIN_BUSY},
+  {6, PIN_DOWNLOAD}
+};
+
+void readSerialBuffer(uint8_t *buffer, int length) {
+  for(int i = 0; i < length; i++) {
+    buffer[i] = (uint8_t) Serial.read();
+  }
+}
+
+void sendResponse(uint8_t apiCode, uint8_t componentCode, uint8_t *data=NULL, uint16_t length=0) {
+  Serial.write(lastClass);
+  Serial.write(lastInstruction);
+  Serial.write(apiCode);
+  Serial.write(componentCode);
+
+  Serial.write((uint8_t*)(&length), 2);
+
+  Serial.write(data, length);
+};
+
+void sendResponseByte(uint8_t apiCode, uint8_t componentCode, uint8_t data) {
+  sendResponse(apiCode, componentCode, &data, 1);
+}
+
+void echoAscending(uint8_t parameter1, uint8_t parameter2, uint16_t length) {
+
+}
+
+void echoDescending(uint8_t parameter1, uint8_t parameter2, uint16_t length) {
+
+}
+
+void echoLengthAscending(uint8_t parameter1, uint8_t parameter2, uint16_t length) {
+  uint8_t response[4]  = {
+    (uint8_t) Serial.read(),
+    0x00,
+  };
+  memcpy(response + 2, &length, 2);
+  sendResponse(0, 0, response, sizeof(response));
+}
+
+void echoIdentity(uint8_t parameter1, uint8_t parameter2, uint16_t length) {
+  uint8_t buf[length];
+  readSerialBuffer(buf, length);
+  sendResponse(0, 0, buf, length);
+}
+
+void readGPIO(uint8_t index, uint8_t, uint16_t) {
+  uint8_t value = digitalRead(pinMapping[index]);
+  sendResponse(0, 0, &value, 1);
+};
+
+void writeGPIO(uint8_t index, uint8_t value, uint16_t) {
+  digitalWrite(pinMapping[index], value);
+  sendResponse(0, 0);
+}
+
+void waitForHigh(uint8_t index, uint8_t, uint16_t) {
+  int pin = pinMapping[index];
+  while(!digitalRead(pin));
+  sendResponse(0, 0);
+}
+
+void waitForLow(uint8_t index, uint8_t, uint16_t) {
+  int pin = pinMapping[index];
+  while(digitalRead(pin));
+  sendResponse(0, 0);
+}
+
+void readLibraryString(uint8_t, uint8_t, uint16_t) {
+  char firmware[] = "0.1 by Daniel Dakhno";
+  sendResponse(0, 0, (uint8_t*) firmware, strlen(firmware));
+}
+
+void readFirmwareString (uint8_t, uint8_t, uint16_t) {
+  char firmware[] = "0.1 by Daniel Dakhno";
+  sendResponse(0, 0, (uint8_t*) firmware, strlen(firmware));
+}
+
+void readFirmwareDate(uint8_t, uint8_t, uint16_t) {
+  char firmware[] = __DATE__;
+  sendResponse(0, 0, (uint8_t*) firmware, strlen(firmware));
+}
+
+void awaitBusyState(int state) {
+  while(digitalRead(PIN_BUSY) != state);
+}
+
+void transmitSPI(uint8_t *data, int length, uint8_t *response, int response_length) {
+  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
+  if(length > 0) {
+    awaitBusyState(LOW);
+
+    digitalWrite(PIN_SS, LOW);
+
+    delay(2);
+
+    for(int i = 0; i < length; i++) {
+      SPI.transfer(data[i]);
+    }
+
+    awaitBusyState(HIGH);
+
+    digitalWrite(PIN_SS, HIGH);
+
+    awaitBusyState(LOW);
+  }
+
+  if(response_length == 0) {
+    SPI.endTransaction();
+    return;
+  }
+
+  memset(response, 0xFF, response_length);
+
+  digitalWrite(PIN_SS, LOW);
+
+  // SPI.transferBytes(stub, response, response_length);
+  for(int i = 0; i < response_length; i++) {
+    response[i] = SPI.transfer(stub[i]);
+  }
+
+  awaitBusyState(HIGH);
+
+  digitalWrite(PIN_SS, HIGH);
+
+  awaitBusyState(LOW);
+
+  SPI.endTransaction();
+  digitalWrite(PIN_SS, HIGH);
+}
+
+void transmit(uint8_t, uint8_t, uint16_t length) {
+  uint8_t buffer[length];
+
+  readSerialBuffer(buffer, length);
+
+  transmitSPI(buffer, length, NULL, 0);
+
+  sendResponse(0, 0);
+}
+
+void transceive(uint8_t, uint8_t, uint16_t length) {
+  uint8_t buffer[length];
+  if(length > 0) {
+    readSerialBuffer(buffer, length);
+  }
+
+  uint8_t response[sizeof(stub)];
+
+  transmitSPI(buffer, length, response, sizeof(response));
+
+  sendResponse(0, 0, response, sizeof(response));
+}
+
+void receive(uint8_t arg1, uint8_t arg2, uint16_t length) {
+  transceive(arg1, arg2, 0);
+}
+
+std::map<uint8_t, classHandler> commandMap {
+  {
+    0x01, { // transceive
+    {
+      {0x05, transmit},
+      {0x0E, receive},
+      {0xFD, transceive},
+    }
+  }},
+  {
+    0x10, { // GPIO
+      {0x0E, readGPIO},
+      {0x05, writeGPIO},
+      {0xA1, waitForHigh},
+      {0xA0, waitForLow},
+    }
+  },
+  {
+    0xFF, { // Configuration
+
+    }
+  },
+  {
+    0x0E, { // Versioning
+      {0x01, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 0); }}, // library major
+      {0x02, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 1); }}, // library minor
+      {0x03, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 1); }}, // dev something,
+      {0x04, readLibraryString},
+
+      {0x11, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 0); }}, // firmware major
+      {0x12, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 0); }}, // firmware minor
+      {0x13, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 1); }}, // firmware dev
+      {0x14, readFirmwareString},
+      {0x15, readFirmwareDate},
+
+      {0x20, [](uint8_t, uint8_t, uint16_t){ sendResponseByte(0, 0, 1); }}, // reader type
+    }
+  },
+  {
+    0x1B, {
+      {0x0A, echoAscending},
+      {0x0D, echoDescending},
+      {0x1A, echoLengthAscending},
+      {0xE0, echoIdentity},
+  }}
+};
+
+void uartOnReceive(){
+  uartBufferAvailable = true;
+}
+
+void setup() {
+  pinMode(PIN_BUSY, INPUT);
+  pinMode(PIN_IRQ, INPUT);
+  pinMode(PIN_DOWNLOAD, OUTPUT);
+  pinMode(PIN_RESET, OUTPUT);
+  pinMode(PIN_SS, OUTPUT);
+
+  memset(stub, 0xFF, sizeof(stub));
+
+  // SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI);
+  SPI.begin();
+
+  Serial.begin(115200);
+
+  // Serial.onReceive(uartOnReceive, true);
+}
+
+void loop() {
+  while(!Serial.available());
+
+  delay(1);
+
+  lastClass = Serial.read();
+  lastInstruction = Serial.read();
+
+  uint8_t parameter1 = Serial.read();
+  uint8_t parameter2 = Serial.read();
+
+  uint16_t payloadLength = 0;
+
+
+  // Serial.read((uint8_t*)(&payloadLength), 2);
+  payloadLength |= Serial.read();
+  payloadLength |= Serial.read() << 8;
+
+  if(payloadLength != Serial.available()) {
+    // error with payload length
+  }
+
+  auto handler = commandMap.find(std::forward<uint8_t>(lastClass));
+
+  if(handler == commandMap.end()) {
+    // error finding class
+  }
+
+  auto insHandler = handler->second.find(std::forward<uint8_t>(lastInstruction));
+
+  if(insHandler == handler->second.end()) {
+    // error finding instruction
+  }
+
+  insHandler->second(parameter1, parameter2, payloadLength);
+}
